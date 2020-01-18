8f0c9c80bb70d1446793f1ce429a4610e5fb9558
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-01-18 14:20:08 +0800

    api done
    parameters newname, pagejson

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: [:show, :update, :destroy]
+  before_action :set_page, only: [:show, :update, :destroy, :change_name]
 
   def show
     render json: @page
   end
   
   def create
-    @page = Page.new(page_params)
+    puts "create"
+    @page = Page.new(pagejson: params[:pagejson], name: params[:newname])
     if @page.save
       render json: @page
     else 
@@ -15,9 +16,10 @@ class PagesController < ApplicationController
   end
 
   def update
+    puts "update"
     if @page
-      if @page.update(page_params) #allow users to change page name?
-        render json: { message: 'Page updated' }, status: 200
+      if @page.update(pagejson: params[:pagejson], name: params[:newname]) #allow users to change page name?
+        render json: @page, status: 200
       else 
         render json: { error: @page.errors.full_messages }, status: 400
       end
@@ -26,12 +28,27 @@ class PagesController < ApplicationController
     end
   end
 
+  # def change_name
+  #   puts "update name"
+  #   if @page
+  #     if params[:newname]
+  #       puts params[:newname]
+  #       @page.update(name: params[:newname])
+  #       render json: @page, status: 200
+  #     else 
+  #       render json: { error: @page.errors.full_messages }, status: 400
+  #     end
+  #   else
+  #     render json: { error: 'Page not found' }, status: 404
+  #   end
+  # end
+
   private
   def set_page
-    @page = Page.find(name: params[:name]) #index by name
+    @page = Page.find_by(name: params[:name]) #index by name
   end
   
   def page_params
-    params.permit(:pagejson, :name)
+    params.permit(:pagejson, :name, :newname)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get :pages, to: "pages#show"
+  patch :pages, to: "pages#update"
+  # patch :change_name, to: "pages#change_name"
   resources :pages
   root to: "static#home"
 end
