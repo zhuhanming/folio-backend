14f604aa2ea8350dd155265be3091ecc79cf5d0a
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-01-18 13:00:07 +0800

    Page api

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,2 +1,37 @@
 class PageController < ApplicationController
+  before_action :set_page, only: [:show, :update, :destroy]
+
+  def show
+    render json: @page
+  end
+  
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      render json: @page
+    else 
+      render json: { error: @page.errors.full_messages }, status: 400
+    end
+  end
+
+  def update
+    if @page
+      if @page.update(page_params) #allow users to change page name?
+        render json: { message: 'Page updated' }, status: 200
+      else 
+        render json: { error: @page.errors.full_messages }, status: 400
+      end
+    else
+      render json: { error: 'Page not found' }, status: 404
+    end
+  end
+
+  private
+  def set_page
+    @page = Page.find(params[:name]) #index by name
+  end
+  
+  def page_params
+    params.permit(:pagejson, :name)
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ApplicationRecord
+    validates_presence_of :name
+    validates_uniqueness_of :name
 end
