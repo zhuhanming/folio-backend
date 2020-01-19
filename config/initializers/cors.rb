c539a40bf335ba5a0c8656f92627fb1e19242561
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-01-19 11:31:21 +0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,5 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "http://localhost:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
-  
+  allow do
+    origins "https://folio-hnr.netlify.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
