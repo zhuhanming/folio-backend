5d32af6ad5cc027b05dc1e281ad17e08c7619538
Hanming Zhu <hanming@huawen.com>
2020-05-22 17:29:55 +0800

    fix: fix netlify url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://folio-hnr.netlify.com"
+    origins "https://folio-hnr.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
